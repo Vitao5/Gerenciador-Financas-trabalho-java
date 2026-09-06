bb8cf5b49a894f44d1bce7ed0bf984b778e91a59
Victor Gabriel <69489641+Vitao5@users.noreply.github.com>
2026-09-06 16:50:30 -0300

    Change database user and password in conexao.java
    Updated database connection credentials in conexao.java.

--- src/main/java/com/mycompany/sistemafinanceiro/util/conexao.java
+++ src/main/java/com/mycompany/sistemafinanceiro/util/conexao.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class conexao {
     public Connection conectarBanco() throws SQLException {
         String url = "jdbc:mysql://127.0.0.1:3306/fiscalize_financas";
-        String usuario = "fiscalize_app";
-        String pass = "fiscalize@2026";
+        String usuario = "usuario";
+        String pass = "senha";
 
         return DriverManager.getConnection(url, usuario, pass);
     }
